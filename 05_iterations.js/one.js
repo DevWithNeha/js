b3c7ece711b956f701616b9423a56fc726a72e59
nehasingh148 <ranikumarisingh2004@gmail.com>
2026-05-27 21:12:39 +0530

    loops break and continue

---
+++ 05_iterations.js/one.js
@@ -0,0 +1,42 @@
+// for
+
+// for(let i = 0;i<=10;i++){
+//     const element = i;
+//     // if(element == 5){
+//     //     console.log("5 is best ele")
+//     // }
+//     console.log(element)
+// }
+
+// for(let i=0;i<=10;i++){
+//     for(let j=0;j<=10;j++){
+//         console.log(`${i}*${j} = ${i*j}`)
+//     }
+// }
+
+// let myArray = ["neha", "manju", "santosh", "ritesh"]
+
+// console.log(myArray.length)
+
+// for(let i=0;i<myArray.length;i++){
+//     console.log(myArray[i])
+// }
+
+// Break and Continue
+
+// for(let i=1;i<=20;i++){
+//     if(i == 5){
+//         console.log(`Detected 5`)
+//         break
+//     }
+//     console.log(`value of i is ${i}`)
+// }
+
+
+for(let i=1;i<=20;i++){
+    if(i == 5){
+        console.log(`Detected 5`)
+        continue
+    }
+    console.log(`value of i is ${i}`)
+}
